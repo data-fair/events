a5f3489074882b83855ed8f521a253b5c9c6f1be
Alban Mouton <alban.mouton@gmail.com>
2024-10-10 16:53:10 +0200

    chore: some build tuning

--- ui/nuxt.config.js
+++ ui/nuxt.config.js
@@ -1,6 +1,5 @@
 import { defineNuxtConfig } from 'nuxt/config'
 
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
   app: {
@@ -8,8 +7,6 @@ export default defineNuxtConfig({
   },
   ignore: process.env.NODE_ENV === 'development' ? [] : ['pages/dev.vue'],
   modules: [
-    // we don't use eslint plugin yet, as edlint-plugin-vuetify is not compatible with eslint 9
-    // see https://github.com/vuetifyjs/eslint-plugin-vuetify/issues/93
     ['@data-fair/lib/nuxt.js', {}]
   ],
   compatibilityDate: '2024-08-30'
