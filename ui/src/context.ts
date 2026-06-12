eb8ade881afac84fdaeb20c6f252a3c8998c550f
Baptiste GUERNY <baptiste.guerny@koumoul.com>
2026-06-12 12:32:45 +0200

    feat(lib-vuetify): add realtime websocket to notification queue (#9)

--- ui/src/context.ts
+++ ui/src/context.ts
@@ -5,4 +5,5 @@ export const $uiConfig = (window as any).__UI_CONFIG as UiConfig
 export const $sitePath = (window as any).__SITE_PATH as string
 export const $cspNonce = (window as any).__CSP_NONCE as string
 export const $apiPath = $sitePath + '/events/api'
+export const $eventsUrl = $sitePath + '/events'
 export const $fetch = ofetch.create({ baseURL: $apiPath })
